d09b4711b59c841bb25fe8c79e1887698b7ed040
fengzhianshang <gaodu328110@163.com>
2015-09-10 14:53:45 +0800

    过滤已经上传的简历

--- app/controllers/suppliers/resumes_controller.rb
+++ app/controllers/suppliers/resumes_controller.rb
@@ -6,16 +6,20 @@ class Suppliers::ResumesController < ApplicationController
   end
 
   def create
-    job = Job.find(params[:job_id])
-    resume = Resume.create!(resume_params)
-    current_supplier.resumes << resume
-    job.delivery!(resume)
+    if Resume.find_by_mobile(params[:resume][:mobile])
+      flash[:error] = '该简历已经被推荐，请换一份简历吧'
+    else
+      job = Job.find(params[:job_id])
+      resume = Resume.create!(resume_params)
+      current_supplier.resumes << resume
+      job.delivery!(resume)
+    end
     redirect_to :back
   end
 
   private
 
   def resume_params
-    params[:resume].permit(:candidate_name, :tag_list, :attachment)
+    params[:resume].permit(:candidate_name, :tag_list, :attachment, :mobile)
   end
 end
