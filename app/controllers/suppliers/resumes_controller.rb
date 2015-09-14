9f4ecabc037546acf784f73f2f2930c222cd925c
zhangzhe <gary.zzhang@gmail.com>
2015-09-14 11:25:52 +0800

    upload resume in resume lib

--- app/controllers/suppliers/resumes_controller.rb
+++ app/controllers/suppliers/resumes_controller.rb
@@ -9,10 +9,12 @@ class Suppliers::ResumesController < Suppliers::BaseController
     if Resume.find_by_mobile(params[:resume][:mobile])
       flash[:error] = '该简历已经被推荐，请换一份简历吧'
     else
-      job = Job.find(params[:job_id])
       resume = Resume.create!(resume_params)
       current_supplier.resumes << resume
-      job.delivery!(resume)
+      if params[:job_id]
+        job = Job.find(params[:job_id])
+        job.delivery!(resume)
+      end
     end
     redirect_to :back
   end
