b1d5b86e87122a3f643b62dbf8b7c1a1cf5fe81e
fengzhianshang <gaodu328110@163.com>
2015-09-17 18:19:45 +0800

    设置默认排序策略

--- app/controllers/admins/base_controller.rb
+++ app/controllers/admins/base_controller.rb
@@ -1,9 +1,15 @@
 class Admins::BaseController < ApplicationController
   helper_method :sort_direction
+  before_action :default_sort, only: [:index]
   layout 'admins'
 
   private
   def sort_direction
     %w(ASC DESC).include?(params[:direction]) ? params[:direction] : 'DESC'
+  end
+
+  def default_sort
+    params[:sort] ||= 'created_at'
+    params[:direction] ||= 'DESC'
   end
 end
