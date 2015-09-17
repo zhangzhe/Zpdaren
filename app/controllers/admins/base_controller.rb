c6025f5a7c0a8d376daff502b134a9235c4ab39c
fengzhianshang <gaodu328110@163.com>
2015-09-17 18:07:29 +0800

    admin job and resume sort

--- app/controllers/admins/base_controller.rb
+++ app/controllers/admins/base_controller.rb
@@ -1,3 +1,9 @@
 class Admins::BaseController < ApplicationController
+  helper_method :sort_direction
   layout 'admins'
+
+  private
+  def sort_direction
+    %w(ASC DESC).include?(params[:direction]) ? params[:direction] : 'DESC'
+  end
 end
