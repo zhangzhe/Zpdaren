6fb097c28fcc99aab08456db9966cf4b69cf2b77
zhangzhe <gary.zzhang@gmail.com>
2015-09-18 16:56:27 +0800

    fix resume count

--- app/controllers/suppliers/deliveries_controller.rb
+++ app/controllers/suppliers/deliveries_controller.rb
@@ -11,7 +11,9 @@ class Suppliers::DeliveriesController < Suppliers::BaseController
   end
 
   def create
-    Delivery.create!(delivery_params)
+    resume = Resume.find(params[:delivery][:resume_id])
+    delivery = Delivery.create!(delivery_params)
+    delivery.update_attributes!(:approved => resume.approved?)
     redirect_to suppliers_job_path(id: params[:delivery][:job_id])
   end
 
