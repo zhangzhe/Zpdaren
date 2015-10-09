15b482c4798be90c5c7521799e141f3d828cc3f4
zhangzhe <gary.zzhang@gmail.com>
2015-10-09 19:09:24 +0800

    structure model

--- config/application.rb
+++ config/application.rb
@@ -1,31 +1,16 @@
 require File.expand_path('../boot', __FILE__)
-
 require 'rails/all'
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Epin
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-
     config.active_record.default_timezone = :local
     config.time_zone = 'Beijing'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
-    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.i18n.default_locale = "zh-CN"
     config.active_record.raise_in_transactional_callbacks = true
     config.middleware.use PDFKit::Middleware, :print_media_type => true
     config.middleware.insert_after ActionDispatch::ParamsParser, ActionDispatch::XmlParamsParser
-    config.i18n.default_locale = "zh-CN"
+    config.autoload_paths += %W(#{config.root}/app/models/money_transfers)
+    config.autoload_paths += %W(#{config.root}/app/models/users)
   end
 end
