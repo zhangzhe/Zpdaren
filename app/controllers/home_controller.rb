584d8955ba63df6fdaa6c343a20fddbed6a55cda
zhangzhe <gary.zzhang@gmail.com>
2015-10-10 14:15:19 +0800

    fix_weixin_subscrib

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,38 +6,20 @@ class HomeController < ApplicationController
 
   require 'digest/sha1'
   SIGNATURE_TOKEN = "12345qwert"
-  # FIXME: deploy steps: 0. remove check_signature; 1. deploy; 2. change callback config in Weixin server; 3. test wixin unsubscribe/subscribe
   def weixin_callback
     if subscribe_condition
       user_id = params["xml"]["EventKey"].split("_").last.to_i
-      user = User.find(user_id)
-      subscribe_user = user.create_weixin(:user_name => params["xml"]["FromUserName"])
-      Weixin.send_subscribe_notification_to(subscribe_user)
-      render :nothing => true
-    elsif unsubscribe_condition
-      weixin = Weixin.where(:user_name => params["xml"]["FromUserName"]).take
-      weixin.destroy
-      render :nothing => true
-    elsif signature_condition
-      signature = Digest::SHA1.hexdigest([SIGNATURE_TOKEN, params[:timestamp], params[:nonce]].sort.join)
-      if signature == params[:signature]
-        render :text => params[:echostr]
-      else
-        false
+      if user_id != 0
+        user = User.find(user_id)
+        # may cause may weixins for one user, fix later
+        subscribe_user = user.create_weixin(:user_name => params["xml"]["FromUserName"])
+        Weixin.send_subscribe_notification_to(subscribe_user)
       end
-    end
-  end
-
-  def check_signature
-    if subscribe_condition
-      user_id = params["xml"]["EventKey"].split("_").last.to_i
-      user = User.find(user_id)
-      subscribe_user = user.create_weixin(:user_name => params["xml"]["FromUserName"])
-      Weixin.send_subscribe_notification_to(subscribe_user)
       render :nothing => true
     elsif unsubscribe_condition
-      weixin = Weixin.where(:user_name => params["xml"]["FromUserName"]).take
-      weixin.destroy
+      if weixin = Weixin.where(:user_name => params["xml"]["FromUserName"]).take
+        weixin.destroy
+      end
       render :nothing => true
     elsif signature_condition
       signature = Digest::SHA1.hexdigest([SIGNATURE_TOKEN, params[:timestamp], params[:nonce]].sort.join)
@@ -51,7 +33,7 @@ class HomeController < ApplicationController
 
   private
   def subscribe_condition
-    params["xml"] && params["xml"]["Event"] == "subscribe" && params["xml"]["EventKey"]
+    params["xml"] && (params["xml"]["Event"] == "subscribe" || params["xml"]["Event"] == "SCAN") && params["xml"]["EventKey"]
   end
 
   def unsubscribe_condition
