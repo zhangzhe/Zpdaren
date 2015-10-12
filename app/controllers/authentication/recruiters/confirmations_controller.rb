d7ab6759cb8735fc218019055f271b28d6b940bf
fengzhianshang <gaodu328110@163.com>
2015-10-12 15:26:29 +0800

    解决qq邮箱注册验证的bug和重复确认注册的bug

--- app/controllers/authentication/recruiters/confirmations_controller.rb
+++ app/controllers/authentication/recruiters/confirmations_controller.rb
@@ -1,3 +1,11 @@
 class Authentication::Recruiters::ConfirmationsController < Devise::ConfirmationsController
   include AuthenticationPathable
+
+  def show
+    if Recruiter.find_by_confirmation_token(params[:confirmation_token]).confirmed_at.nil?
+      super
+    else
+      redirect_to new_recruiter_session_path
+    end
+  end
 end
--- app/controllers/authentication/suppliers/confirmations_controller.rb
+++ app/controllers/authentication/suppliers/confirmations_controller.rb
@@ -1,4 +1,11 @@
 class Authentication::Suppliers::ConfirmationsController < Devise::ConfirmationsController
   include AuthenticationPathable
 
+  def show
+    if Supplier.find_by_confirmation_token(params[:confirmation_token]).confirmed_at.nil?
+      super
+    else
+      redirect_to new_supplier_session_path
+    end
+  end
 end
