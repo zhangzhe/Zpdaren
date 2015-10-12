f19df278bc824384eea2c10e12df0884a6f50f04
fengzhianshang <gaodu328110@163.com>
2015-10-12 15:57:18 +0800

    修改用户注册后，邮箱确认代码

--- app/controllers/authentication/suppliers/confirmations_controller.rb
+++ app/controllers/authentication/suppliers/confirmations_controller.rb
@@ -2,10 +2,11 @@ class Authentication::Suppliers::ConfirmationsController < Devise::Confirmations
   include AuthenticationPathable
 
   def show
-    if Supplier.find_by_confirmation_token(params[:confirmation_token]).confirmed_at.nil?
-      super
-    else
+    if Supplier.find_by_confirmation_token(params[:confirmation_token]).confirmed_at.present?
+      flash[:success] = '已经确认，请重新登录.'
       redirect_to new_supplier_session_path
+    else
+      super
     end
   end
 end
