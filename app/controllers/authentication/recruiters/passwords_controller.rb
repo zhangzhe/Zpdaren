fca40da42b87bd45cbfacce2e8226b1ce16ac08b
zhangzhe <gary.zzhang@gmail.com>
2015-10-14 13:14:58 +0800

    fix password forget bug

--- app/controllers/authentication/recruiters/passwords_controller.rb
+++ app/controllers/authentication/recruiters/passwords_controller.rb
@@ -1,7 +1,3 @@
 class Authentication::Recruiters::PasswordsController < Devise::PasswordsController
-  include AuthenticationPathable
-
-  def after_sending_reset_password_instructions_path_for(resource_name)
-    home_path
-  end
+  include PasswordForgetable
 end
