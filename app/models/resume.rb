0a75be9e34206bf7441c526f471c5c0e42d15a83
zhangzhe <gary.zzhang@gmail.com>
2015-10-20 16:26:25 +0800

    I18N fix from Edwin

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -2,12 +2,11 @@ class Resume < ActiveRecord::Base
   has_many :deliveries
   has_many :jobs, through: :deliveries
   belongs_to :supplier
-  validates_presence_of :candidate_name, :mobile, :tag_list
+  validates_presence_of :candidate_name, :mobile, :tag_list, :email
   validates_presence_of :attachment, message: '不能为空'
   validates_length_of :candidate_name, maximum: 10
   validates_uniqueness_of :mobile, :email, message: '系统中已经存在，请上选择其他候选人'
   validates_length_of :mobile, is: 11
-  validates_length_of :message, maximum: 50
 
   after_create :auto_deliver
 
