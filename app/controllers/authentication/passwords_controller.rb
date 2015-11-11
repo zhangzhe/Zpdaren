7f49843796fed0e052c8ebd07390efc9fd12647b
zhangzhe <gary.zzhang@gmail.com>
2015-11-11 16:43:12 +0800

    fix forget password

--- app/controllers/authentication/passwords_controller.rb
+++ app/controllers/authentication/passwords_controller.rb
@@ -9,4 +9,23 @@ class Authentication::PasswordsController < Devise::PasswordsController
       redirect_to forget_password_path_for(resource_name)
     end
   end
+
+  def update
+    self.resource = resource_class.reset_password_by_token(resource_params)
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      resource.unlock_access! if unlockable?(resource)
+      if Devise.sign_in_after_reset_password
+        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+        set_flash_message(:notice, flash_message) if is_flashing_format?
+        sign_in(resource_name, resource)
+      else
+        set_flash_message(:notice, :updated_not_active) if is_flashing_format?
+      end
+      redirect_to root_path
+    else
+      respond_with resource
+    end
+  end
 end
