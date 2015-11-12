980be8a9e12246e35a803c0e85e2c20f90397e55
fengzhianshang <gaodu328110@163.com>
2015-11-12 16:13:18 +0800

    删除pdf的软连接

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :use_sudo, false
 set :tmp_dir, '/data/Epin/shared/tmp'
 set :rails_env, "production"
 set :linked_files, fetch(:linked_files, []).push('config/database.yml')
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/sitemaps uploads public/pdfjs}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/sitemaps uploads}
 
 # after 'deploy:publishing', 'deploy:start'
 
