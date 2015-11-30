2472fbba8075754cc960d24918576c6d0a223f78
zhangzhe <gary.zzhang@gmail.com>
2015-11-30 16:59:06 +0800

    redirect to page before signin

--- app/controllers/authentication/sessions_controller.rb
+++ app/controllers/authentication/sessions_controller.rb
@@ -1,4 +1,5 @@
 class Authentication::SessionsController < Devise::SessionsController
+
   def create
     self.resource = warden.authenticate!(auth_options)
     set_flash_message(:notice, :signed_in) if is_flashing_format?
@@ -8,5 +9,11 @@ class Authentication::SessionsController < Devise::SessionsController
       flash[:notice] += "您可以点击左下角的微信图标放大并关注我们的微信，以后您相关的账号变动会直接发送到您的微信上。"
     end
     respond_with resource, location: after_sign_in_path_for(resource)
+  end
+
+  def after_sign_in_path_for(resource)
+    previous_url = session[:previous_url]
+    session[:previous_url] = nil
+    previous_url || super
   end
 end
