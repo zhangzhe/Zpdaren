878536e0beccadf3dd0c24ab6b3acf111a784f2c
fengzhianshang <gaodu328110@163.com>
2015-12-04 01:39:45 +0800

    将通知邮件中简历的pdf展示改为提供网址下载

--- lib/epin_cipher.rb
+++ lib/epin_cipher.rb
@@ -1,13 +1,13 @@
 module EpinCipher
-  KEY = 'xiaoda@zpdaren.c0m'
-  IV = 'Xiaoda.c0m@zpdaren'
+  KEY = 'xiaodazpdarenc0m'
+  IV = 'Xiaodac0mzpdaren'
   def self.aes128_encrypt(data)
     aes = OpenSSL::Cipher::AES.new("128-CBC")
     aes.encrypt
     aes.key = KEY
     aes.iv = IV
     r = aes.update(data) + aes.final
-    Base64.encode64(r)
+    Base64.urlsafe_encode64(r)
   end
 
   def self.aes128_decrypt(data)
@@ -15,7 +15,7 @@ module EpinCipher
     aes.decrypt
     aes.key = KEY
     aes.iv = IV
-    r = Base64.decode64(data)
+    r = Base64.urlsafe_decode64(data)
     aes.update(r) + aes.final
   end
 end
