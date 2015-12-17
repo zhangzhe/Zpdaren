bab764e9eee063e7986929c917541746c9a67a48
fengzhianshang <gaodu328110@163.com>
2015-12-17 17:16:51 +0800

    添加报错邮件通知，修改发送邮件代码

--- app/mailers/recruiter_mailer.rb
+++ app/mailers/recruiter_mailer.rb
@@ -1,20 +1,40 @@
 class RecruiterMailer < ApplicationMailer
-  default :from => "Zpdaren<xiaohua@hellohuohua.com>"
 
   def job_approved(recruiter, job)
     @recruiter = recruiter
     @job = job
-    mail(:to => recruiter.email, :subject => "招聘信息成功发布【众聘】", :template_path => "notifications" , :template_name => "job_approved")
+    i = 0
+    begin
+      from = Settings.email.accounts[i]
+      mail(:from => "#{Settings.email.sender}<#{from.user_name}>", to: recruiter.email, subject: "招聘信息成功发布【众聘】", :template_path => "notifications" , :template_name => "job_approved", delivery_method_options: delivery_options(from))
+    rescue Net::SMTPFatalError
+      i += 1
+      retry if i < Settings.email.accounts.size
+    end
   end
 
   def resume_recommended(recruiter, delivery)
     @recruiter = recruiter
     @delivery = delivery
-    mail(:to => recruiter.email, :subject => "新的简历推荐【众聘】", :template_path => "notifications" , :template_name => "resume_recommended")
+    i = 0
+    begin
+      from = Settings.email.accounts[i]
+      mail(:from => "#{Settings.email.sender}<#{from.user_name}>", to: recruiter.email, subject: "新的简历推荐【众聘】", :template_path => "notifications" , :template_name => "resume_recommended", delivery_method_options: delivery_options(from))
+    rescue Net::SMTPFatalError
+      i += 1
+      retry if i < Settings.email.accounts.size
+    end
   end
 
   def job_updated(job)
     @job = job
-    mail(:to => job.recruiter.email, :subject => "招聘信息为您做出微调并成功发布【众聘】", :template_path => "notifications" , :template_name => "job_updated")
+    i = 0
+    begin
+      from = Settings.email.accounts[i]
+      mail(:from => "#{Settings.email.sender}<#{from.user_name}>", to: job.recruiter.email, subject: "招聘信息为您做出微调并成功发布【众聘】", :template_path => "notifications" , :template_name => "job_updated", delivery_method_options: delivery_options(from))
+    rescue Net::SMTPFatalError
+      i += 1
+      retry if i < Settings.email.accounts.size
+    end
   end
 end
