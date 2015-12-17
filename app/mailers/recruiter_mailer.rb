985521e82421add7fb41b8495d6d081fefda7ef8
fengzhianshang <gaodu328110@163.com>
2015-12-17 17:38:14 +0800

    重构给招聘方发邮件的代码

--- app/mailers/recruiter_mailer.rb
+++ app/mailers/recruiter_mailer.rb
@@ -3,35 +3,33 @@ class RecruiterMailer < ApplicationMailer
   def job_approved(recruiter, job)
     @recruiter = recruiter
     @job = job
-    i = 0
-    begin
-      from = Settings.email.accounts[i]
-      mail(:from => "#{Settings.email.sender}<#{from.user_name}>", to: recruiter.email, subject: "招聘信息成功发布【众聘】", :template_path => "notifications" , :template_name => "job_approved", delivery_method_options: delivery_options(from))
-    rescue Net::SMTPFatalError
-      i += 1
-      retry if i < Settings.email.accounts.size
-    end
+    subject = '招聘信息成功发布【众聘】'
+    template_name = 'job_approved'
+    send_mail(recruiter.email, subject, template_name)
   end
 
   def resume_recommended(recruiter, delivery)
     @recruiter = recruiter
     @delivery = delivery
-    i = 0
-    begin
-      from = Settings.email.accounts[i]
-      mail(:from => "#{Settings.email.sender}<#{from.user_name}>", to: recruiter.email, subject: "新的简历推荐【众聘】", :template_path => "notifications" , :template_name => "resume_recommended", delivery_method_options: delivery_options(from))
-    rescue Net::SMTPFatalError
-      i += 1
-      retry if i < Settings.email.accounts.size
-    end
+    subject = '新的简历推荐【众聘】'
+    template_name = 'resume_recommended'
+    send_mail(recruiter.email, subject, template_name)
   end
 
   def job_updated(job)
     @job = job
+    subject = '招聘信息为您做出微调并成功发布【众聘】'
+    template_name = 'job_updated'
+    send_mail(job.recruiter.email, subject, template_name)
+  end
+
+  private
+
+  def send_mail(recipient, subject, template_name, template_path = 'notifications')
     i = 0
     begin
       from = Settings.email.accounts[i]
-      mail(:from => "#{Settings.email.sender}<#{from.user_name}>", to: job.recruiter.email, subject: "招聘信息为您做出微调并成功发布【众聘】", :template_path => "notifications" , :template_name => "job_updated", delivery_method_options: delivery_options(from))
+      mail(:from => "#{Settings.email.sender}<#{from.user_name}>", to: recipient, subject: subject, :template_path => template_path , :template_name => template_name, delivery_method_options: delivery_options(from))
     rescue Net::SMTPFatalError
       i += 1
       retry if i < Settings.email.accounts.size
