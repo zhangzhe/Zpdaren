c13c1fd6cd4fab83d4dacc2c85610db8040bef08
fengzhianshang <gaodu328110@163.com>
2015-12-26 17:31:10 +0800

    修改公司地址为必填

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,10 +1,8 @@
 class Company < ActiveRecord::Base
   belongs_to :recruiter, :foreign_key => :user_id
 
-  validates_presence_of :name, on: :update
-  validates_presence_of :description, on: :update
+  validates_presence_of :name, :description, :mobile, :address, on: :update
   validates_length_of :name, on: :update, maximum: 50
-  validates_presence_of :mobile, on: :update
   validates_presence_of :service_protocol, on: :update, unless: Proc.new { |company| company.service_protocol? }
 
   scope :active, -> { where.not('name' => nil) }
