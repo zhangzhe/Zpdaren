7901adf3cfdc4c18a9939a2cb67ab80693403613
zhangzhe <gary.zzhang@gmail.com>
2015-12-28 18:47:50 +0800

    ajax comment(80%)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,25 @@
 class CommentsController < ActionController::Base
 
   def create
-    update_user_info_if_login_user
-    update_guest_name_in_cookies
-    if params[:comment][:parent_id].to_i > 0
-      parent = Comment.find_by_id(params[:comment].delete(:parent_id))
-      comment = parent.children.build(comment_params)
-    else
-      comment = Comment.new(comment_params)
-    end
-    @interview = Interview.find(params[:interview_id])
-    comment.interview_id = params[:interview_id]
-    if comment.save
-      redirect_to @interview, notice: '回复失败'
-    else
-      flash[:error] = comment.errors.full_messages.first
-      redirect_to @interview, error: '回复失败，请填写好回复内容'
+    respond_to do |format|
+      update_user_info_if_login_user
+      update_guest_name_in_cookies
+      if params[:comment][:parent_id].to_i > 0
+        @parent = Comment.find_by_id(params[:comment].delete(:parent_id))
+        @comment = @parent.children.build(comment_params)
+      else
+        @comment = Comment.new(comment_params)
+      end
+      @interview = Interview.find(params[:interview_id])
+      @comment.interview_id = params[:interview_id]
+      if @comment.save
+        format.html { redirect_to @interview, notice: '回复失败' }
+        format.js   {}
+        format.json { render json: @comment, status: :created, location: @comment }
+      else
+        format.html { redirect_to @interview, error: '回复失败，请填写好回复内容' }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
     end
   end
 
