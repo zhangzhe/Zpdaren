08b1bb4d73a71173ff42e9208f3017821307b1df
zhangzhe <gary.zzhang@gmail.com>
2016-01-03 17:32:00 +0800

    tag conditions

--- app/controllers/admins/tags_controller.rb
+++ app/controllers/admins/tags_controller.rb
@@ -1,7 +1,8 @@
 class Admins::TagsController < Admins::BaseController
 
   def index
-    @tags = Job.tag_counts_on(:tags).order('taggings_count DESC')
-
+    tags = Job.tag_counts_on(:tags).order('taggings_count DESC')
+    @nomal_tags = tags.where(priority: [nil, 0])
+    @priority_tags = tags.where(:priority => 1)
   end
 end
