c00b8fb25ff8d8e830d544e8599420e1e6fd95f8
zhangzhe <gary.zzhang@gmail.com>
2016-01-04 14:04:09 +0800

    improve ajax

--- app/controllers/admins/tags_controller.rb
+++ app/controllers/admins/tags_controller.rb
@@ -11,7 +11,10 @@ class Admins::TagsController < Admins::BaseController
       tag_name = params[:tag].strip
       tag = Tag.find_by_name(tag_name)
       if tag.update_attributes!(:priority => params[:priority].to_i)
-        format.json { render json: tag, status: 200 }
+        tags = Job.tag_counts_on(:tags).order('taggings_count DESC')
+        @nomal_tags = tags.where(priority: [nil, 0])
+        @priority_tags = tags.where(:priority => 1)
+        format.js {}
       else
         format.json { render json: tag, status: 500 }
       end
