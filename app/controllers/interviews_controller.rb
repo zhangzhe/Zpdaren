f27044f5ab789fc2bf7de544de117af23fe3b1ad
zhangzhe <gary.zzhang@gmail.com>
2016-01-07 17:14:23 +0800

    tmp again

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -3,14 +3,13 @@ class InterviewsController < ActionController::Base
 
   def show
     if request.user_agent =~ /MicroMessenger/ # visit from weixin
-      if current_user
+      if current_user # step 3
         @interview = Interview.find(params[:id])
-      elsif params["code"] && (params["state"] == "STATE")
+      elsif params["code"] && (params["state"] == "STATE") # step 2
         session[:return_to] = request.path
         redirect_to "/wechat_oauth/callback?code=#{params[:code]}&state=#{params[:state]}"
-      else
-        session[:return_to] = request.url
-        redirect_to "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx0e604319f74644b3&redirect_uri=http%3A%2F%2F673df419.ngrok.io/interviews/#{params[:id]}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect"
+      else # step 1
+        redirect_to "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx0e604319f74644b3&redirect_uri=http%3A%2F%2F4dbf96b1.ngrok.io/interviews/#{params[:id]}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect"
       end
     else
       @interview = Interview.find(params[:id])
--- app/controllers/wechat_oauth_controller.rb
+++ app/controllers/wechat_oauth_controller.rb
@@ -2,11 +2,14 @@ class WechatOauthController < ApplicationController
   def callback
     response = Wechat.conn.get '/sns/oauth2/access_token', { :appid => Wechat.mp_appid, :secret => Wechat.mp_secret, :code => params[:code], :grant_type => "authorization_code" }
     remote_data = JSON(response.body)
-    user = Commenter.where(provider: "wechat", uid: remote_data["unionid"], email: "#{remote_data['unionid']}@wechat.com").first_or_create
+    user = Commenter.where(provider: "wechat", uid: remote_data["unionid"]).first_or_create
+    user.update_attributes!(:email => "#{remote_data["unionid"]}@wechat.com", :password => remote_data["unionid"]) unless user.persisted?
+
     response = Wechat.conn.get '/sns/userinfo', { :access_token => remote_data["access_token"], :openid => remote_data["openid"] }
     remote_data = JSON(response.body)
     commenter_detail = user.commenter_detail || user.build_commenter_detail
     commenter_detail.update_from_remote!(remote_data)
+
     sign_in(user)
     redirect_to(session[:return_to] || "/")
     session[:return_to] = nil
