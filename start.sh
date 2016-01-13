98262d42dc23a60fad109a9304dc377e20ab859b
fengzhianshang <gaodu328110@163.com>
2016-01-13 19:38:33 +0800

    delete sidekiq start command -d

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ cd /usr/src/app
 
 bundle exec rake db:migrate RAILS_ENV=production
 
-service nginx start && bundle exec unicorn_rails -E production -c config/unicorn.rb && bundle exec sidekiq -e production -d
+service nginx start && bundle exec unicorn_rails -E production -c config/unicorn.rb && bundle exec sidekiq -e production
