961274981f7f75c06d7927f332725e5c5429f7d8
zhangzhe <gary.zzhang@gmail.com>
2016-01-13 10:53:50 +0800

    use find instead of find_by_id

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,7 +1,7 @@
 class InterviewsController < ActionController::Base
   layout 'anonymous_job'
   def show
-    @interview = Interview.find_by_id(params[:id])
+    @interview = Interview.find(params[:id])
     if @interview.available? || (current_user && current_user.admin?)
       if request.user_agent =~ /MicroMessenger/ # visit from weixin
         if current_user # step 3
