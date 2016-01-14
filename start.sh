d52193a702dd08fefe210ef911f4775c280b29de
fengzhianshang <gaodu328110@163.com>
2016-01-14 16:28:08 +0800

    running rails domain

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ bundle exec rake db:migrate RAILS_ENV=production
 
 echo `service nginx start`
 
-echo `bundle exec unicorn_rails -E production -c config/unicorn.rb`
+echo `bundle exec unicorn_rails -E production -c config/unicorn.rb -D`
 
 echo `bundle exec sidekiq -e production`
