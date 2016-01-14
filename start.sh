ae162f509805f2f04090567ef6aa6f8911eb8f03
fengzhianshang <gaodu328110@163.com>
2016-01-14 16:47:16 +0800

    修改启动命令执行的格式

--- start.sh
+++ start.sh
@@ -4,8 +4,4 @@ cd /usr/src/app
 
 bundle exec rake db:migrate RAILS_ENV=production
 
-echo `service nginx start`
-
-echo `bundle exec unicorn_rails -E production -c config/unicorn.rb -D`
-
-echo `bundle exec sidekiq -e production`
+service nginx start && bundle exec unicorn_rails -E production -c config/unicorn.rb -D && bundle exec sidekiq -e production
