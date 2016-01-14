1699560b28fe2dc40e984d1d3efb9b96890e5b43
fengzhianshang <gaodu328110@163.com>
2016-01-14 16:08:02 +0800

    show start.sh log

--- start.sh
+++ start.sh
@@ -4,4 +4,8 @@ cd /usr/src/app
 
 bundle exec rake db:migrate RAILS_ENV=production
 
-service nginx start && bundle exec unicorn_rails -E production -c config/unicorn.rb && bundle exec sidekiq -e production
+echo `service nginx start`
+
+echo `bundle exec unicorn_rails -E production -c config/unicorn.rb`
+
+echo `bundle exec sidekiq -e production`
