944188984ee871a59d497b7ab360e9f1536ec356
fengzhianshang <gaodu328110@163.com>
2016-01-14 11:23:58 +0800

    修改start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,4 @@ cd /usr/src/app
 
 bundle exec rake db:migrate RAILS_ENV=production
 
-bundle exec sidekiq -e production
-
-service nginx start && bundle exec unicorn_rails -E production -c config/unicorn.rb
+service nginx start && bundle exec unicorn_rails -E production -c config/unicorn.rb && bundle exec sidekiq -e production -C config/sidekiq.yml
