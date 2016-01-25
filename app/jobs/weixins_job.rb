e9cbf41288d1826635615840cd4875290099b830
fengzhianshang <gaodu328110@163.com>
2016-01-25 12:04:23 +0800

    修改发送微信消息的bug

--- app/jobs/weixins_job.rb
+++ app/jobs/weixins_job.rb
@@ -2,6 +2,6 @@ class WeixinsJob < ActiveJob::Base
   queue_as :weixins
 
   def perform(args)
-    WeixinApi::Notification.send(args[:event], args[:id])
+    Weixin.send(args[:event], args[:id])
   end
 end
